1c23dfa089fd8c8fd1d59b5b2b0255922eec9930
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-07-23 10:08:46 +0530

    intial commit

---
+++ BackendFitShark/src/main/java/com/example/backendfitshark/BackendFitSharkApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendfitshark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFitSharkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendFitSharkApplication.class, args);
+    }
+
+}
---
+++ BackendFitShark/src/test/java/com/example/backendfitshark/BackendFitSharkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendfitshark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFitSharkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
