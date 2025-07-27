ea45ab137a95052c78c5a17035b9d5ae0284e6ff
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-07-27 21:25:12 +0530

    l1

--- Frontend/src/pages/home.jsx
+++ Frontend/src/pages/home.jsx
@@ -1,13 +1,118 @@
-
+import "../assets/css/bootstrap.min.css"
+import "../assets/css/font-awesome.css"
+import "../assets/css/templatemo-training-studio.css"
+import "../assets/js/jquery-2.1.0.min.js"
+//import "../assets/js/popper.js"
+import "../assets/js/bootstrap.min.js"
+import "../assets/js/scrollreveal.min.js"
+//import "../assets/js/waypoints.min.js"
+import "../assets/js/jquery.counterup.min.js"
+import "../assets/js/imgfix.min.js"
+import "../assets/js/mixitup.js"
+import "../assets/js/accordions.js"
+import "../assets/js/custom.js"
+import { useState } from "react"
+import axios from "axios"
+import Navbar3 from "../components/navbar3.jsx"
 
 function Home ()
 {
  
     return(
+<>
+<Navbar3/>
+ <section class="section" id="features">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3">
+                    <div class="section-heading">
+                        <h2>Welcome, <em>Mr. Pasan !</em></h2>
+                       <br></br>
+                        <p>"Training Studio is free CSS template for gyms and fitness centers. You are allowed to use this layout for your business website."</p>
+                    <div class="main-button scroll-to-section">
+                         <br></br>
+                            <a href="#our-classes">Log Activity</a>
+                        </div>
+                    
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <ul class="features-items">
+                        <li class="feature-item">
+                            <h2>Past Sessions</h2>
+                        </li>
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="assets/images/features-first-icon.png" alt="second one"/>
+                            </div>
+                            <div class="right-content">
+                                <h4>New Gym Training</h4>
+                                <p>If you wish to support TemplateMo website via PayPal, please feel free to contact us. We appreciate it a lot.</p>
+                                <a href="#" class="text-button">Discover More</a>
+                            </div>
+                        </li>
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="assets/images/features-first-icon.png" alt="third gym training"/>
+                            </div>
+                            <div class="right-content">
+                                <h4>Basic Muscle Course</h4>
+                                <p>Credit goes to <a rel="nofollow" href="https://www.pexels.com" target="_blank">Pexels website</a> for images and video background used in this HTML template.</p>
+                                <a href="#" class="text-button">Discover More</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-6">
+                    <ul class="features-items">
+                        <li class="feature-item">
+                            
+                        </li>
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="assets/images/features-first-icon.png" alt="training fifth"/>
+                            </div>
+                            <div class="right-content">
+                                <h4>Yoga Training</h4>
+                                <p>This template is built on Bootstrap v4.3.1 framework. It is easy to adapt the columns and sections.</p>
+                                <a href="#" class="text-button">Discover More</a>
+                            </div>
+                        </li>
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="assets/images/features-first-icon.png" alt="gym training"/>
+                            </div>
+                            <div class="right-content">
+                                <h4>Body Building Course</h4>
+                                <p>Suspendisse fringilla et nisi et mattis. Curabitur sed finibus nisi. Integer nibh sapien, vehicula et auctor.</p>
+                                <a href="#" class="text-button">Discover More</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
 
-<h1>Dashboard</h1>
+    <section class="section" id="call-to-action">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 offset-lg-1">
+                    <div class="cta-content">
+                        <h2>Don’t <em>think</em>, begin <em>today</em>!</h2>
+                        <p>Ut consectetur, metus sit amet aliquet placerat, enim est ultricies ligula, sit amet dapibus odio augue eget libero. Morbi tempus mauris a nisi luctus imperdiet.</p>
+                        <div class="main-button scroll-to-section">
+                            <a href="#our-classes">Become a member</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
+
+</>
     )
 
 
